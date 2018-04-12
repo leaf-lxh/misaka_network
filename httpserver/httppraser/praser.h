af9fd0e22ed197872a480dd78e610113d6f99930
leaf-lxh <meleaf@qq.com>
2018-04-12 18:38:57 +0800

    added some functions

--- httpserver/httppraser/praser.h
+++ httpserver/httppraser/praser.h
@@ -1,5 +1,19 @@
 #include <string>
 
 class httppraser{
+public :
+	
+	size_t HandleRequest(const std::string &receive,std::string &response);
+	struct key_value
+	{
+		std::string key;
+		std::string value;
+	};
+private :
+	bool ReadFile(const char *path,std::string &buffer);
+	bool WriteFile(const char *path,std::string &buffer);
 
+	int FormatRequestHeaders (const std::string &receive);
+	int CreateReponseHeaders(const char *status_code);
+	size_t CreateResponseContent(const char *file_name, std::string &content_buffer);
 };
