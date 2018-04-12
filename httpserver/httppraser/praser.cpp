9d16f81449ba502863c9c400e6cccad6f15c75b8
leaf-lxh <meleaf@qq.com>
2018-04-12 18:38:13 +0800

    added some functions

--- httpserver/httppraser/praser.cpp
+++ httpserver/httppraser/praser.cpp
@@ -1,1 +1,33 @@
 #include <string>
+#include <fstream>
+
+
+size_t httppraser::HandleRequest(const std::string &receive,std::string &response)
+{
+	size_t response_length = 0;
+	int count_headers = FormatRequestHeaders(receive);
+	return response_length;
+}
+
+int httppraser::FormatRequestHeaders (const std::string &receive)
+{
+	int count_header = 0;
+	return count_header;
+}
+
+size_t httppraser::CreateResponseContent(const char *file_name, std::string &content_buffer)
+{
+	size_t content_length = 0;
+	
+	return content_length;
+}
+
+bool ReadFile(const char *path,std::string &buffer)
+{
+	return true;
+}
+
+bool WriteFile(const char *path,std::string &buffer)
+{
+	return true;
+}
