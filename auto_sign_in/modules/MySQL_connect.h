e2e00d7e5c0e7a0155f19293b66cb65e6d5b9eec
leaf_lxh <leaf_12516>
2018-04-21 20:33:02 +0800

    ...

---
+++ auto_sign_in/modules/MySQL_connect.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+
+class mysql_connect
+{
+public:
+	int mysql_connect_init(std::string &username, std::string &password);
+
+private:
+
+};
+
+
