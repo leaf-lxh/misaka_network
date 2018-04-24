b130d8d37040d43a8cb61dcf52f85700212d8e69
leaf_lxh <leaf_12516>
2018-04-24 19:16:28 +0800

    implemented functions: ReadConfig() WriteConfig()  WriteLog()

--- auto_sign_in/modules/FileIO.cpp
+++ auto_sign_in/modules/FileIO.cpp
@@ -4,14 +4,17 @@
 #include "FileIO.h"
 #include <sstream>
 #include <fstream>
+#include <ctime>
+
+#include <iostream>
+
 
 bool SignInFileIO::ReadConfig(ConfigStruct & cs)
 {
-	//初始化配置结构
+	
 	cs.hour = -1;
 	cs.minute = -1;
 
-	//判断目录是否存在,不存在则创建，创建不了则返回false
 	if (!access(config_path.c_str(), F_OK))
 	{
 		if (!mkdir(config_path.c_str(), 644))
@@ -20,19 +23,17 @@ bool SignInFileIO::ReadConfig(ConfigStruct & cs)
 		}
 	}
 
-
-	//准备读取文件
 	std::fstream file;
 	std::string FileContent = "";
-	file.open(config_path+config_name, std::fstream::app);//使用append模式，如果文件不存在也可以创建
+	file.open(config_path+config_name, std::fstream::in);
 
-	std::stringstream ss;
+	
 	if (!file.good())
 	{
 		return false;
 	}
 
-	//逐行读取配置，并存储到传进来的ConfigStruct中去
+	std::stringstream ss;
 	while (getline(file, FileContent))
 	{
 		
@@ -50,7 +51,7 @@ bool SignInFileIO::ReadConfig(ConfigStruct & cs)
 			ss >> cs.minute;
 		}
 	}
-	//如果读取的数据有问题则设置为默认时间
+
 	if (cs.hour<0 || cs.hour>23)
 	{
 		WriteLog("Config.hour setting error. Changed to defult value 2");
@@ -63,7 +64,6 @@ bool SignInFileIO::ReadConfig(ConfigStruct & cs)
 		cs.minute = 0;
 	}
 	
-	//关闭打开的文件并将保存配置
 	file.close();
 	WriteConfig(cs);
 	return true;
@@ -71,10 +71,61 @@ bool SignInFileIO::ReadConfig(ConfigStruct & cs)
 
 bool SignInFileIO::WriteConfig(ConfigStruct & cs)
 {
-	return false;
+	if (!access(config_path.c_str(), F_OK))
+	{
+		if (!mkdir(config_path.c_str(), 644))
+		{
+			return false;
+		}
+	}
+
+	std::fstream file;
+	std::string FileContent = "";
+	file.open(config_path + config_name, std::fstream::out);
+
+	if (!file.good())
+	{
+		return false;
+	}
+
+	file << "hour=" << cs.hour << std::endl;
+	file<<"minute=" << cs.minute << std::flush;
+
+	file.close();
+
+	return true;
 }
 
 bool SignInFileIO::WriteLog(const std::string & log)
 {
-	return false;
+	
+
+	if (!access(log_path.c_str(), F_OK))
+	{
+		if (!mkdir(log_path.c_str(), 644))
+		{
+			return false;
+		}
+	}
+
+	std::fstream file;
+
+	file.open(log_path + log_name, std::fstream::out | std::fstream::app);
+
+	if (!file.good())
+	{
+		return false;
+	}
+
+	time_t CurrentTime = time(NULL);
+
+	std::string TimeString = ctime(&CurrentTime);
+	TimeString =  TimeString.substr(0, TimeString.length()-1);
+
+	std::cout << TimeString << ": " << log << std::endl;
+	file << TimeString << ": " << log << std::endl;
+
+	file.close();
+
+	return true;
 }
