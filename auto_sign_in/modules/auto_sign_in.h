d2fe659c6b6b77debec16a2928a29365e4b25271
leaf-lxh <meleaf@qq.com>
2018-09-18 12:33:48 +0800

    ...

--- auto_sign_in/modules/auto_sign_in.h
+++ auto_sign_in/modules/auto_sign_in.h
@@ -5,18 +5,31 @@
 class SignIn
 {
 public:
+
+	//本类的构造函数
 	SignIn(std::string BDUSS, std::string STOKEN);
-	std::string BDUSS;
-	std::string STOKEN;
-    //签到所有贴吧，
-    void SendSignInRequest();
+	//本类的析构函数
+	~SignIn();
 
-	//查询未签到的贴吧,返回kw列表
-	std::vector<std::string> GetUnsignedList();
+	
+	/************************************************************************************************************************
+	*发送签到请求
+	*参数：UserBDUSS                   | 将进行签到的用户的BDUSS
+	*          kwList                          | 需要签到的贴吧列表
+	*返回：std::vector<std::string> | 签到失败的贴吧kw列表
+	*************************************************************************************************************************/
+   std::vector<std::string> SendSignInRequest(std::string UserBDUSS, std::vector<std::string> kwList);
 
+	/************************************************************************************************************************
+	*查询用户关注的贴吧
+	*参数：UserBDUSS                                  | 用户的BDUSS
+	*返回：std::vector<string>                      | 用户关注的贴吧的列表
+	*************************************************************************************************************************/
+	std::vector<std::string> GetUnsignedList(std::string UserBDUSS);
+	
 private:
     
-    int GetILikeTiebaList();
+    
     bool GetFID(std::string kw);
     bool GetTBS(std::string kw);
     bool sign(std::string kw);
