26ba199dd388526dbd8037dbe21bc201c0744cff
leaf-lxh <meleaf@qq.com>
2018-09-24 20:31:43 +0800

    Update sql statement

--- auto_sign_in/create.sql
+++ auto_sign_in/create.sql
@@ -1,12 +1,11 @@
-create database SignInUserInfo character set utf8;
+create database if not exist SignInUserInfo character set utf8;
 
 use SignInUserInfo;
 
-create table baidu_user_info
+create table if not exist baidu_user_info
 (
 	id int unsigned not null auto_increment primary key,
 	BDUSS TEXT(300) not null,
-	TIEBA_STOKEN TEXT(200) not null,
 	TIEBA_USERNAME char(20) not null,
 	E_MAIL TEXT(200) 
 );
