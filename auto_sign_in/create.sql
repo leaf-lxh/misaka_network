0300bebb91e598d58bb39143a6916129f0b92630
leaf-lxh <meleaf@qq.com>
2018-10-05 17:41:02 +0800

    修复了两处SQL语句错误

--- auto_sign_in/create.sql
+++ auto_sign_in/create.sql
@@ -1,8 +1,8 @@
-create database if not exist SignInUserInfo character set utf8;
+create database if not exists SignInUserInfo character set utf8;
 
 use SignInUserInfo;
 
-create table if not exist baidu_user_info
+create table if not exists baidu_user_info
 (
 	id int unsigned not null auto_increment primary key,
 	BDUSS TEXT(300) not null,
