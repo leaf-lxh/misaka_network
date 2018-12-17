f1d2d3bfe86b763ef73fb47a9cf8b79897f96fe1
leaf-lxh <meleaf@qq.com>
2018-12-17 19:03:29 +0800

    changed comments

--- httpserver/src/modules/FileIO.cpp
+++ httpserver/src/modules/FileIO.cpp
@@ -1,15 +1,3 @@
-/******************************************************************************************
-FileName: FileIO.cpp
-Author: leaf-lxh     Version:1.0    Date:2018-12-9
-Description: 进行文件的读写、判断某个路径是否存在，进行某个路径的创建
-Version: 1.0 2018-12-9
-Function List: 
-	1.PathCheck() 检查路径是否存在
-	2.PathCreate() 创建路径
-	3.FileIO::WriteLog() 写日志到文件中
-	4.FileIO::ReadFile() 读取文件
-	5.FileIO::WriteFile() 写文件
-******************************************************************************************/
 #include "FileIO.h"
 
 #include <fstream>
@@ -18,13 +6,8 @@ Function List:
 #include <sys/types.h>
 #include <sys/stat.h>
 
-/******************************************************************************************
-*To read file content and store it to a string buffer.
-* @path: The path of the file that to be readed.
-* @buffer: The string buffer that will store file content.
-*return value: no 
-******************************************************************************************/
-bool FileIO::ReadFile(const std::string &path, std::string &buffer)
+
+bool FileIO::ReadFile(const std::string path, std::string &buffer)
 {
 	if (!PathCheck(path))
 	{
@@ -59,15 +42,8 @@ bool FileIO::ReadFile(const std::string &path, std::string &buffer)
 	file.close();
 }
 
-/******************************************************************************************
-*Writting given content to a specific file.
-* @path: The path of the file that to be writed.
-* @content: The content that will be writed to the file.
-* @isReWrite: If this paramter is true, then the file will be over writed with the content that given.
-*             Othervise the content will be appended to the original file content.
-*return value: nothing.
-******************************************************************************************/
-void FileIO::WriteFile(const std::string &path, std::string &content ,bool isReWrite)
+
+bool FileIO::WriteFile(const std::string path, std::string &content ,bool isReWrite)
 {
 	if (!PathCheck(path))
 	{
@@ -93,12 +69,18 @@ void FileIO::WriteFile(const std::string &path, std::string &content ,bool isReW
 	file.close();
 }
 
-/******************************************************************************************
-*To check the directory whether exist or not. Not working for existed file. 
-* @path: The path that to be checked.
-*return value: If the directory exists, return true. 
-               Otherwise return false.
-******************************************************************************************/
+bool FileIO::WriteLog(const std::string path, const std::string &log)
+{
+	time_t currentUnixTime = time(NULL);
+	std::string output = asctime(localtime(&currentUnixTime));
+	output = output.substr(0, output.length() - 1);//remove \0 that created by asctime()
+	output += ": " + log + '\n';
+
+	WriteFile(path, output, false);
+
+}
+
+
 bool PathCheck(const std::string & path)
 {
 	if (!access(path.c_str(), F_OK))
@@ -112,12 +94,7 @@ bool PathCheck(const std::string & path)
 	}
 }
 
-/******************************************************************************************
-*Create directory. Make sure the path is vaild before calling this function
-* @path: The path that want to create,
-*return value: If the creating succeed,return true.
-               Otherwise return false.
-******************************************************************************************/
+
 bool PathCreate(const std::string & path)
 {
 	std::string currentPath;
@@ -149,20 +126,6 @@ bool PathCreate(const std::string & path)
 	}
 }
 
-/******************************************************************************************
-*Writing log in the form of "time: log\n".
-* @path: log path.
-* @log: A string that contents log.
-*return value: nothing.
-******************************************************************************************/
-void FileIO::WriteLog(const std::string &path, const std::string &log)
-{
-	time_t currentUnixTime = time(NULL);
-	std::string output = asctime(localtime(&currentUnixTime));
-	output = output.substr(0, output.length() - 1);//remove \0 that created by asctime()
-	output += ": " + log + '\n';
 
-	WriteFile(path, output,false);
-	
-}
+
 
--- httpserver/src/modules/FileIO.h
+++ httpserver/src/modules/FileIO.h
@@ -1,3 +1,9 @@
+/******************************************************************************************
+FileName: FileIO.h
+Author: leaf-lxh
+Description: 进行文件的读写、判断某个路径是否存在，进行某个路径的创建
+******************************************************************************************/
+
 #include <string>
 
 /******************************************************************************************
@@ -21,9 +27,34 @@ class FileIO
 public:
 	FileIO() = default;
 
-	bool WriteLog(const std::string &path, const std::string &log);
-	void ReadFile(const std::string &path, std::string &buffer);
-	void WriteFile(const std::string &path, std::string &content, bool isReWrite);
+
+	/******************************************************************************************
+	*Writing log in the form of "time: log\n".
+	* @path: log path.
+	* @log: A string that contents log.
+	*return value: nothing.
+	******************************************************************************************/
+	bool WriteLog(const std::string path, const std::string &log);
+
+
+	/******************************************************************************************
+	*To read file content and store it to a string buffer.
+	* @path: The path of the file that to be readed.
+	* @buffer: The string buffer that will store file content.
+	*return value: no
+	******************************************************************************************/
+	bool ReadFile(const std::string path, std::string &buffer);
+
+
+	/******************************************************************************************
+	*Writting given content to a specific file.
+	* @path: The path of the file that to be writed.
+	* @content: The content that will be writed to the file.
+	* @isReWrite: If this paramter is true, then the file will be over writed with the content that given.
+	*             Othervise the content will be appended to the original file content.
+	*return value: nothing.
+	******************************************************************************************/
+	bool WriteFile(const std::string path, std::string &content, bool isOverWrite);
 	
 };
 
