f6866d79bae31075010f115fbd5951a9ec0829c4
leaf-lxh <meleaf@qq.com>
2019-09-22 15:08:35 +0800

    移动tinyHttpd目录

---
+++ LXHBlogSpace/back-end/library/tinyHttpd/example/example.cpp
@@ -0,0 +1,31 @@
+#include "../tinyHttpd.h"
+#include <iostream>
+#include <signal.h>
+TinyHttpd server;
+void sigterm(int signal)
+{
+	server.~TinyHttpd();
+	exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	signal(SIGTERM, sigterm);
+	using namespace std;
+	try
+	{
+		server.Init();
+		server.LoadMIME();
+		server.StartListen();
+		server.DumpProperty();
+	}
+	catch (std::runtime_error e)
+	{
+		cout << "Server init error: " << e.what() << endl;
+	}
+	
+	cout << "Server start running." << endl;
+	server.StartHandleRequest();
+
+	return 0;
+}
